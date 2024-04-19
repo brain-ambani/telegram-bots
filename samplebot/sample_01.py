0343c9d9ef968a231cf02feddfd305f460298912
brian-ambani <brainambani1@gmail.com>
2024-04-19 19:26:01 +0300

    completed the simple bot

--- samplebot/sample_01.py
+++ samplebot/sample_01.py
@@ -6,8 +6,10 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 
 from datetime import datetime
 
-TOKEN: Final = os.getenv('API_SECRET')
 
+
+# API_KEY = os.getenv('API_KEY')
+TOKEN: Final = '6433473951:AAE2G-qUToFCjejhLO59fAIQMn5Ge7IorF4'
 BOT_USERNAME: Final = '@simple_chappie_bot'
 
 
@@ -55,11 +57,11 @@ def handle_response(text: str) -> str:
 
 # Message Handlers
 
-async def handle_message(update: Update, context: ContextTypes):
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text
 
-    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+    print(f'User({update.message.chat.id}) in {message_type}: "{text}"')
 
     if message_type == 'group':
         if BOT_USERNAME in text:
@@ -73,8 +75,7 @@ async def handle_message(update: Update, context: ContextTypes):
     print('Bot:', response)
     await update.message.reply_text(response)
 
-
-async def error_handler(update: Update, context: ContextTypes):
+async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {Update} caused error: {context.error}')
 
 
@@ -89,7 +90,7 @@ if(__name__ == '__main__'):
     app.add_handler(CommandHandler('custom', custom_command))
 
     #Messages
-    app.add_handler(MessageHandler(filters.Text, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
     #Errors
     app.add_error_handler(error_handler)
@@ -98,3 +99,4 @@ if(__name__ == '__main__'):
     print('Polling...')
 
     app.run_polling(poll_interval=3)
+
